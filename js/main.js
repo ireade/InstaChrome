1273d9cd5cab4f61c3ad6aca8ce07132b167aceb
Ire Aderinokun <ire@ireaderinokun.com>
2015-09-02 17:59:38 +0100

    local storage

--- js/main.js
+++ js/main.js
@@ -2,43 +2,52 @@
 
 $(document).ready(function() {
 
-	function apiRequest() {
+	var request = function() {
 
-		var cleanTag;
+		chrome.storage.local.get("tag", function(result) {
 
-		if ( tag.indexOf("#") === -1 ) {
-			cleanTag = tag;
-		} else {
-			cleanTag = tag.split('#')[1];
-		}
+			var tag = result.tag;
+			var cleanTag;
 
-		cleanTag = cleanTag.replace(/\s/g, "");
-		
-		var requestUrl = 'https://api.instagram.com/v1/tags/'+cleanTag+'/media/recent?access_token='+accessToken+'&callback=?';
-
-		$.getJSON(requestUrl, {}, function(data) {
-
-			var items = [];
-
-			for (var i=0; i < 6; i++) {
-				items.push(data.data[i]);
-			}
-
-			var source = $('#grams-template').html();
-			var template = Handlebars.compile(source);
-			var output = template( {Grams: items} );
-
-			
-			if (data.data.length === 0) {
-				$('.loading').hide();
-				$("#grams").html('<p class="error">Sorry, nothing turned up! Please try again</p>')
-				
+			if ( tag.indexOf("#") === -1 ) {
+				cleanTag = tag;
+				$('input[type="text"]').val('#'+tag);
 			} else {
-				$('.loading').hide();
-				$("#grams").html(output).fadeIn();
+				cleanTag = tag.split('#')[1];
 			}
-		});
-	}
+
+
+			cleanTag = cleanTag.replace(/\s/g, "");
+			
+			var requestUrl = 'https://api.instagram.com/v1/tags/'+cleanTag+'/media/recent?access_token='+accessToken+'&callback=?';
+
+			$.getJSON(requestUrl, {}, function(data) {
+
+				var items = [];
+
+				for (var i=0; i < 6; i++) {
+					items.push(data.data[i]);
+				}
+
+				var source = $('#grams-template').html();
+				var template = Handlebars.compile(source);
+				var output = template( {Grams: items} );
+
+				
+				if (data.data.length === 0) {
+					$('.loading').hide();
+					$("#grams").html('<p class="message">Sorry, nothing turned up! Please try again</p>')
+					
+				} else {
+					$('.loading').hide();
+					$("#grams").html(output).fadeIn();
+				}
+			});
+
+
+		}); // end get local storage
+
+	}; // end apiRequest
 
 	$('#search').on('submit', function() {
 
@@ -46,17 +55,34 @@ $(document).ready(function() {
 		$('.loading').show();
 
 		var searchInput = $('#searchText').val();
-		tag = searchInput;
-		apiRequest();
+
+
+		chrome.storage.local.set({"tag" : searchInput}, function(){
+
+			request();
+
+		});
 
 		return false;
 	})
 
-	// Set Default
-	var tag = '#yolo';
-	apiRequest();
 
 
-	$('input[type="text"]').val(tag);
+	chrome.storage.local.get("sertag", function(result) {
+
+		if ( result.tag ) {
+
+			request();
+			$('input[type="text"]').val(result.tag);
+
+		} else {
+
+			$('.loading').hide();
+			$("#grams").html('<p class="message">Welcome to InstaChrome!</p>')
+			
+		}
+
+	})
+
 
 });

70840090e633200aa1cb7eb7cdd87fa732e24688
Ire Aderinokun <ire@ireaderinokun.com>
2015-09-04 09:19:39 +0100

    minor improvements

---
+++ README.md
@@ -0,0 +1,5 @@
+# InstaChrome
+
+An extension for chrome that displays Instagram posts from a tag of your choice.
+
+If you want to use this, you need to add your own Instagram access token and store it in `js/access-token.js`.
--- js/main.js
+++ js/main.js
@@ -9,6 +9,7 @@ $(document).ready(function() {
 			var tag = result.tag;
 			var cleanTag;
 
+			// Display # on the front end, but remove # for the cleanTag variable
 			if ( tag.indexOf("#") === -1 ) {
 				cleanTag = tag;
 				$('input[type="text"]').val('#'+tag);
@@ -18,6 +19,7 @@ $(document).ready(function() {
 
 
 			cleanTag = cleanTag.replace(/\s/g, "");
+
 			
 			var requestUrl = 'https://api.instagram.com/v1/tags/'+cleanTag+'/media/recent?access_token='+accessToken+'&callback=?';
 
@@ -25,19 +27,24 @@ $(document).ready(function() {
 
 				var items = [];
 
+				// Get only 6 items
 				for (var i=0; i < 6; i++) {
 					items.push(data.data[i]);
 				}
 
+				// Setup Handlebars templating
 				var source = $('#grams-template').html();
 				var template = Handlebars.compile(source);
 				var output = template( {Grams: items} );
 
+
 				$('.loading').hide();
 
 				if (data.data.length === 0) {
+					// Show error messages if no images for this tag
 					$('.message-error').show();
 				} else {
+					// Else, display the images in the template
 					$("#grams").html(output).fadeIn();
 				}
 			});
@@ -48,15 +55,17 @@ $(document).ready(function() {
 	}; // end request
 
 	
-
+	// When users search
 	$('#search').on('submit', function() {
 
+		// Clear messages and any previous images, and show loading animation
 		$('.message').hide();
 		$("#grams").html("");
 		$('.loading').show();
 
 		var searchInput = $('#searchText').val();
 
+		// Set the search text to local storage
 		chrome.storage.local.set({"tag" : searchInput}, function(){
 			request();
 		});
@@ -66,7 +75,7 @@ $(document).ready(function() {
 
 
 
-
+	// On load of the new tab, check if there is any tag previously stored
 	chrome.storage.local.get("tag", function(result) {
 
 		if ( result.tag ) {
